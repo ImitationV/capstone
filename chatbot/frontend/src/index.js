07ec7dd21631e2ba5bde9df62267b44f8d9db587
Mansur-Mahdee <116994740+Mansur-Mahdee@users.noreply.github.com>
2025-04-22 01:57:32 -0400

    Delete chatbot directory

--- chatbot/frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Chatbot from "./Chatbot";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-ReactDOM.render(<Chatbot />, document.getElementById("root"));
-
